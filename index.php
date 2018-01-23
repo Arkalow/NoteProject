6a2909b980e4169a99f8ec0e00b6971d286fe05a
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-01-23 12:24:25 +0100

    Affichage des Notes

--- index.php
+++ index.php
@@ -29,34 +29,21 @@
         <div class="col s9">
             <div class="row">
                 <div class="col s12">
-                    <div class="divider"></div>
-                    <div class="section">
-                        <h5>Note 1</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ducimus, nobis eos sunt in voluptates ex tempora laudantium? Quos, voluptatem. Aperiam iste voluptatibus pariatur quis labore sit et, corrupti debitis ipsa minus suscipit cum neque nemo perferendis, eligendi vitae ea in porro saepe doloribus! Quaerat unde earum dignissimos minima sint officiis vitae quis amet. Dolorem culpa officia ut perspiciatis pariatur nesciunt ducimus. Deleniti, repudiandae animi, quo hic, commodi illo numquam expedita qui praesentium quia consequatur reiciendis ut! Dolorem, quo maxime dolor ipsam alias commodi sit! Magnam sunt exercitationem perferendis impedit quasi, beatae cumque quaerat minima. Itaque beatae velit delectus soluta?</p>
-                    </div>
-                    <div class="divider"></div>
-                    <div class="section">
-                        <h5>Note 2</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ducimus, nobis eos sunt in voluptates ex tempora laudantium? Quos, voluptatem. Aperiam iste voluptatibus pariatur quis labore sit et, corrupti debitis ipsa minus suscipit cum neque nemo perferendis, eligendi vitae ea in porro saepe doloribus! Quaerat unde earum dignissimos minima sint officiis vitae quis amet. Dolorem culpa officia ut perspiciatis pariatur nesciunt ducimus. Deleniti, repudiandae animi, quo hic, commodi illo numquam expedita qui praesentium quia consequatur reiciendis ut! Dolorem, quo maxime dolor ipsam alias commodi sit! Magnam sunt exercitationem perferendis impedit quasi, beatae cumque quaerat minima. Itaque beatae velit delectus soluta?</p>
-                    </div>
-                    <div class="divider"></div>
-                    <div class="section">
-                        <h5>Note 3</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ducimus, nobis eos sunt in voluptates ex tempora laudantium? Quos, voluptatem. Aperiam iste voluptatibus pariatur quis labore sit et, corrupti debitis ipsa minus suscipit cum neque nemo perferendis, eligendi vitae ea in porro saepe doloribus! Quaerat unde earum dignissimos minima sint officiis vitae quis amet. Dolorem culpa officia ut perspiciatis pariatur nesciunt ducimus. Deleniti, repudiandae animi, quo hic, commodi illo numquam expedita qui praesentium quia consequatur reiciendis ut! Dolorem, quo maxime dolor ipsam alias commodi sit! Magnam sunt exercitationem perferendis impedit quasi, beatae cumque quaerat minima. Itaque beatae velit delectus soluta?</p>
-                    </div>
+                    <main>
+                    </main>
                 </div>
               </div>
     
               <div class="row">
     
                 <div class="col s4">
-                    <button><i class="material-icons large">add</i></button>
+                    <a><i class="material-icons large">add</i></a>
                 </div>
                 <div class="col s4">
-                    <button><i class="material-icons large">create</i></button>
+                    <a><i class="material-icons large">create</i></a>
                 </div>
                 <div class="col s4">
-                    <button><i class="material-icons large">remove</i></button>
+                    <a><i class="material-icons large">remove</i></a>
                 </div>
         </div>
   
--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 jQuery(function($){
     $.get('http://localhost/NoteProject/NoteProject/JSON.php')
         .done(function(data, textStatus, jqXHR){
-            console.log(data)
+            $main = $('main')
+            for(var i = 0; i < data.length; i++){
+                var $section = $('<div>')
+                $main.append($('<div>').addClass('divider'))
+                $section.addClass('section').append($('<h5>').text(data[i].name))
+                $section.addClass('section').append($('<p>').text(data[i].content))
+                $main.append($section)
+            }
+            
         })
         .fail(function(jqXHR, textStatus){
             console.log('Erreur 404')
